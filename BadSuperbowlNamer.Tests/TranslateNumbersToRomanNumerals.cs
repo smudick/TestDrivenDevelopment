56247445406200e9e9bd158db90d8f7169243d28
Sam Mudick <smudick@gmail.com>
2021-04-05 21:03:56 -0500

    monday night class 4.5

---
+++ BadSuperbowlNamer.Tests/TranslateNumbersToRomanNumerals.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+
+namespace BadSuperbowlNamer.Tests
+{
+    //name the class for the things we're testing
+    public class TranslateNumbersToRomanNumerals // translate_numbers_to_roman_numerals
+    {
+        [Fact] //this method is a test
+        public void when_the_number_1_is_passed_I_should_be_returned() //must be public, and return void, and take no parameters
+        {
+            //Arrange -- context creation
+            var numberToConvert = 1;
+            var translator = new RomanNumeralTranslator();
+
+            //Act -- the action being taken in a given context
+            var result = translator.NumberToNumeral(numberToConvert);
+
+            //Assert  -- an empty test is a passing test, make sure to not leave empty tests in production
+            Assert.Equal("I", result);
+        }
+
+        [Fact]
+        public void when_the_number_5_is_passed_V_should_be_returned()
+        {
+            //Arrange
+            var numberToConvert = 5;
+            var translator = new RomanNumeralTranslator();
+            var expectedResult = "V";
+
+            //Act
+            var result = translator.NumberToNumeral(numberToConvert);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }  
+        [Fact]
+        public void when_the_number_9_is_passed_IX_should_be_returned()
+        {
+            //Arrange
+            var numberToConvert = 9;
+            var translator = new RomanNumeralTranslator();
+            var expectedResult = "IX";
+
+            //Act
+            var result = translator.NumberToNumeral(numberToConvert);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ BadSuperbowlNamer/RomanNumeralTranslator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadSuperbowlNamer
+{
+    public class RomanNumeralTranslator
+    {
+        public string NumberToNumeral(int numberToConvert)
+        {
+            //throw new NotImplementedException(); // for when you just want to have a non-functioning method
+
+            //var result = "";
+
+            var result = numberToConvert switch // switch expression
+            {
+                1 => "I",
+                5 => "V",
+                9 => "IX"
+            };
+
+            //switch (numberToConvert)
+            //{
+            //    case 1: 
+            //    result += "I";
+            //        break;
+            //    case 5:
+            //        result += "V";
+            //        break;
+            //    case 9:
+            //        result += "IX";
+            //        break;
+            //}
+           
+            return result;
+        }
+    }
+}
